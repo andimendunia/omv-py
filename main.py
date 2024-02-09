e5c5a5ac4a3232194e784278fa8d95e2ef27c418
Andi Permana <andi.thea96@gmail.com>
2024-02-09 08:27:46 +0700

    Ubah pagi-pagi

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ os.environ['WXSUPPRESS_SIZER_FLAGS_CHECK'] = '1'
 conn = sqlite3.connect('omv.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS operators (id INTEGER PRIMARY KEY, name TEXT)''')
+# c.execute('''CREATE TABLE IF NOT EXISTS records (id INTEGER PRIMARY KEY, name TEXT)''')
 conn.commit()
 
 class frameMain(frMain):
@@ -17,47 +18,54 @@ class frameMain(frMain):
         # Insiasi parent class
         frMain.__init__(self, parent)
 
-        # Waktu bergerak
+        # Tanggal dan waktu bergerak
         self.timer = wx.Timer(self)
         self.Bind(wx.EVT_TIMER, self.updateTime, self.timer)
         self.timer.Start(1000)
 
     def updateTime(self, event):
-        #Format tanggal dan waktu bergerak
+        # Format tanggal dan waktu bergerak
         currentTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         
-        # Update waktu bergerak ke status bar
+        # Update status bar yang di bawah
         self.sbMain.SetStatusText("Ready", 0)
         self.sbMain.SetStatusText("COM3", 1)
         self.sbMain.SetStatusText("BAUD 9600", 2)
         self.sbMain.SetStatusText(currentTime, 3)
+
+#### KODE UNTUK TAB HOME ####
         
     def btnHomeColOnClick(self, event):
         # Buka dialog pilih warna
         dialog = dgColors(self)
         if dialog.ShowModal() == wx.ID_OK:
-            # Handle dialog OK result here, if necessary
+            # Eksekusi pilih warna ketika OK
             pass
         dialog.Destroy()
         # No need to call event.Skip() unless you have a specific reason to continue event propagation
 
+#### KODE UNTUK TAB HISTORY ####
+
+#### KODE UNTUK TAB OPERATOR ####
+        
     def btnOpSaveOnClick(self, event):
         name = self.tcOpName.GetValue().strip()
         if not len(name) > 0:
             wx.MessageBox(f"Name cannot be empty.", "Error", wx.OK | wx.ICON_ERROR)
         else:
-            # Check if the name already exists in the database
+            # Periksa nama jika sudah ada di database terlepas besar kecil huruf
             c.execute("SELECT * FROM operators WHERE LOWER(name) = LOWER(?)", (name,))
             if c.fetchone():
-                # If the name exists, show a message box and do not insert
+                # Jika nama sudah ada, beri pesan error, tidak disimpan.
                 wx.MessageBox(f"Name '{name}' already exists.", "Error", wx.OK | wx.ICON_ERROR)
             else:
-                # If the name does not exist, insert it into the database
+                # Jika nama tidak ada, simpan ke database.
                 c.execute("INSERT INTO operators (name) VALUES (?)", (name,))
                 conn.commit()
-                # Show a success message
+                # Tampilkan pesan sukses
                 wx.MessageBox(f"Name '{name}' successfully saved", "Saved", wx.OK | wx.ICON_INFORMATION)
-            
+
+#### MENYALAKAN APLIKASI ####            
 
 class MainApp(wx.App):
     def OnInit(self):
